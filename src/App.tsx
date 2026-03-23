ed3c4543aace59fcf05569dda0aeb5cb067d8ae6
haZya <63403456+haZya@users.noreply.github.com>
2026-03-23 18:35:16 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <main>
+      <section id="header">
+
+      </section>
+      <section id="body">
+
+      </section>
+    </main>
+  )
+}
+
+export default App
